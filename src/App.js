08763cb15e09730b6074f51819f8c721f71798fd
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-03-29 12:40:50 +0300

    updated to react router v6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
-import { useAuthContext } from './hooks/useAuthContext'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // styles
-import './App.css'
+import './App.css';
 
 // pages & components
-import Dashboard from './pages/dashboard/Dashboard'
-import Create from './pages/create/Create'
-import Login from './pages/login/Login'
-import Signup from './pages/signup/Signup'
-import Project from './pages/project/Project'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import OnlineUsers from './components/OnlineUsers'
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Project from './pages/project/Project';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
 
 function App() {
-  const { authIsReady, user } = useAuthContext()
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className="App">
@@ -24,28 +24,28 @@ function App() {
           {user && <Sidebar />}
           <div className="container">
             <Navbar />
-            <Switch>
-              <Route exact path="/">
-                {!user && <Redirect to="/login" />}
-                {user && <Dashboard />}
-              </Route>
-              <Route path="/create">
-                {!user && <Redirect to="/login" />}
-                {user && <Create />}
-              </Route>
-              <Route path="/projects/:id">
-                {!user && <Redirect to="/login" />}
-                {user && <Project />}
-              </Route>
-              <Route path="/login">
-                {user && <Redirect to="/" /> }
-                {!user && <Login /> }
-              </Route>
-              <Route path="/signup">
-                {user && user.displayName && <Redirect to="/" /> }
-                {!user && <Signup /> }
-              </Route>
-            </Switch>
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/projects/:id"
+                element={user ? <Project /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+              />
+              <Route
+                path="/signup"
+                element={user ? <Navigate to="/" /> : <Signup />}
+              />
+            </Routes>
           </div>
           {user && <OnlineUsers />}
         </BrowserRouter>
@@ -54,4 +54,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
